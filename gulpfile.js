bb0d413a4b3b8340bbc23b2f2911e5b9ad8dd791
Jan Gosmann <jgosmann@uwaterloo.ca>
2019-02-13 20:16:11 +0100

    Add favicon.

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const changed = require('gulp-changed');
+const favicons = require('favicons').stream;
+const fs = require('fs');
+const gulp = require('gulp');
+
+gulp.task('favicons', () => {
+  const dest = './static';
+  const metafile = './assets/meta.html';
+  return gulp.src('./assets/svg/favicon.svg')
+    .pipe(changed(dest, {transformPath: _ => { return metafile; }}))
+    .pipe(favicons({
+      appName: 'Jan\' outdoor adventures',
+      developerName: 'Jan Gosmann',
+      developerURL: 'https://www.jgosmann.de',
+      start_url: '/',
+      icons: {
+        appleStartup: false,
+        coast: false,
+        firefox: false,
+        yandex: false
+      }
+    }, (html) => {
+      fs.writeFile(
+        metafile, html.join(''),
+        (err) => {
+          if (err) {
+            return console.log(err);
+          }
+        }
+      );
+    }))
+    .pipe(gulp.dest(dest));
+});
