72b29948ac02ff8b5b322892e6d4c5f65847b588
Jan Gosmann <jgosmann@uwaterloo.ca>
2019-04-04 10:16:56 +0200

    Implement dynamic loading of pictures.

--- assets/js/index.js
+++ assets/js/index.js
@@ -87,4 +87,24 @@ const showLocation = (targetId, latLng, zoomLevel) => {
   leaflet.marker(latLng).addTo(map);
 }
 
-export { showGpxTrack, showLocation }
+const intersectionOptions = {
+  rootMargin: '100px',
+}
+const loadHtml = {}
+const loadNow = (entries, observer) => {
+  entries.filter(entry => entry.isIntersecting).forEach(entry => {
+    intersectionObserver.unobserve(entry.target)
+    const newEl = document.createElement('picture')
+    newEl.innerHTML = loadHtml[entry.target.getAttribute('id')]
+    entry.target.parentNode.replaceChild(newEl, entry.target)
+  })
+}
+const intersectionObserver = new IntersectionObserver(loadNow, intersectionOptions)
+
+const registerDynLoad = (targetId, html) => {
+  const target = document.getElementById(targetId)
+  loadHtml[targetId] = html
+  intersectionObserver.observe(target)
+}
+
+export { registerDynLoad, showGpxTrack, showLocation }
