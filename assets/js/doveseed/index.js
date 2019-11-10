b9238fb64b0ac9a07b2ad0c724a4ca81b767e6e4
Jan Gosmann <jan@hyper-world.de>
2019-11-10 16:42:23 +0100

    Add ReCAPTCHA for subscriptions

--- assets/js/doveseed/index.js
+++ assets/js/doveseed/index.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faCircleNotch, faTimes } from '@fortawesome/free-solid-svg-icons';
 import React, { useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
+import ReCAPTCHA from "react-google-recaptcha";
 
 function VCollapsible(props) {
   var className = 'vcollapsible';
@@ -31,17 +32,33 @@ function ProcessingStateIcon(props) {
 function SubscribeForm(props) {
   const [state, setState] = useState('initial');
   const emailInput = useRef(null);
+  const recaptcha = useRef(null);
 
   const onSubmit = (e) => {
     e.preventDefault();
     setState('requestOngoing');
 
-    fetch(props.url + emailInput.current.value, {method: 'POST'}).then((response) => {
+    recaptcha.current.execute();
+  };
+
+  const onCaptchaComplete = () => {
+    body = {
+      captcha: recaptcha.current.getValue()
+    };
+
+    fetch(props.url + emailInput.current.value, {
+      method: 'POST',
+      body: JSON.stringify(body)})
+    .then((response) => {
       setState(response.ok ? 'success' : 'error');
     }).catch(() => {
       setState('error');
     });
   };
+
+  const onError = () => {
+    setState('error')
+  }
 
   const disabled = state === 'requestOngoing' || state === 'success';
 
@@ -54,6 +71,13 @@ function SubscribeForm(props) {
         </VCollapsible>
         {props.submitLabel}
       </button>
+      <ReCAPTCHA
+        ref={recaptcha}
+        size="invisible"
+        sitekey="6LdL4sEUAAAAAEsmXbX_Z21wla_bM9dhjSe8I5hf"
+        onChange={onCaptchaComplete}
+        onExpired={onError}
+        onErrored={onError} />
     </form>
   );
 }
