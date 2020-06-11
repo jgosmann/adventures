bbfd3a56f6a26355b53b38ee3af1df7916f0aade
Jan Gosmann <jan@hyper-world.de>
2020-06-11 22:08:37 +0200

    Compare checksum on deployment to reduce network traffic

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -avz --delete public/ jgosmann@hyper-world.de:~/adventures
+rsync -avz --delete --checksum public/ jgosmann@hyper-world.de:~/adventures
 ssh jgosmann@hyper-world.de passenger-config restart-app /var/customers/webs/jgosmann/adventures-search
