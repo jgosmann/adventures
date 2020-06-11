5cb9848978526cd29bb0faa75071b7a5977cb1e4
Jan Gosmann <jan@hyper-world.de>
2020-06-11 16:54:04 +0200

    Reload search index after deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 rsync -avz --delete public/ jgosmann@hyper-world.de:~/adventures
+ssh jgosmann@hyper-world.de passenger-config restart-app /var/customers/webs/jgosmann/adventures-search
