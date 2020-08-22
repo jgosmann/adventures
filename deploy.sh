2685ad465e2537412c63f974ccad62eb593505ef
Jan Gosmann <jan@hyper-world.de>
2020-08-22 19:26:46 +0200

    Deploy to netlify

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-rsync -avz --delete public/ jgosmann@hyper-world.de:~/adventures
+#rsync -avz --delete public/ jgosmann@hyper-world.de:~/adventures
+netlify deploy --prod
 ssh jgosmann@hyper-world.de passenger-config restart-app /var/customers/webs/jgosmann/adventures-search
