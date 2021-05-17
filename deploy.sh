636c6e4c1c39a719381762f16071503787475fed
Jan Gosmann <jan@hyper-world.de>
2021-05-17 20:39:10 +0200

    Use local install of netlify-cli

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #rsync -avz --delete public/ jgosmann@hyper-world.de:~/adventures
-netlify deploy --prod -d public/
+npx netlify deploy --prod -d public/
 ssh jgosmann@hyper-world.de passenger-config restart-app /var/customers/webs/jgosmann/adventures-search
