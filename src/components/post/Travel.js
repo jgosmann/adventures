88497b4257b5b069ab8cd86a0dc3f2e2d6f7d311
Jan Gosmann <jan@hyper-world.de>
2021-08-27 17:32:26 +0200

    Allow to set Travel zoom level

--- src/components/post/Travel.js
+++ src/components/post/Travel.js
@@ -6,13 +6,27 @@ import Geodesic from "../leaflet/Geodesic"
 import Map from "../leaflet/Map"
 import Marker from "../leaflet/Marker"
 
-const Travel = ({ route, markIntermediate }) => {
+const centerForZoomedViewport = route => {
+  const latitudes = route.map(([lat]) => lat)
+  const longitudes = route.map(([, long]) => long)
+  return [
+    (Math.min(...latitudes) + Math.max(...latitudes)) / 2.0,
+    (Math.min(...longitudes) + Math.max(...longitudes)) / 2.0,
+  ]
+}
+
+const Travel = ({ route, markIntermediate, zoom }) => {
+  const viewport = zoom
+    ? {
+        center: centerForZoomedViewport(route),
+        zoom,
+      }
+    : {
+        bounds: route,
+        padding: [50, 50],
+      }
   return (
-    <Map
-      css={{ height: 600, margin: "32px 0" }}
-      bounds={route}
-      padding={[50, 50]}
-    >
+    <Map css={{ height: 600, margin: "32px 0" }} {...viewport}>
       <Geodesic latlngs={route} color={colors.red} />
       {markIntermediate ? (
         route.map((stop, i) => <Marker key={i} position={stop} />)
@@ -29,6 +43,7 @@ const Travel = ({ route, markIntermediate }) => {
 Travel.propTypes = {
   route: PropTypes.array.isRequired,
   markIntermediate: PropTypes.bool,
+  zoom: PropTypes.number,
 }
 
 export default Travel
