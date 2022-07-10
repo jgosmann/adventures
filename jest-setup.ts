61716ab8d8ad593a35f5b09719528063c19e85e7
Jan Gosmann <jan@hyper-world.de>
2022-07-10 20:56:13 +0200

    Add msw for testing

--- jest-setup.ts
+++ jest-setup.ts
@@ -2,7 +2,14 @@ import "@testing-library/jest-dom"
 import "isomorphic-fetch"
 import ResizeObserver from "resize-observer-polyfill"
 import { matchers } from "@emotion/jest"
+import { server } from "./src/mocks/server"
 
 expect.extend(matchers)
 
 global.ResizeObserver = ResizeObserver
+
+beforeAll(() => server.listen())
+
+afterEach(() => server.resetHandlers())
+
+afterAll(() => server.close())
