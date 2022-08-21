bc289f92b290d4b4fdb4217a2d765eda9e3a98cc
Jan Gosmann <jan@hyper-world.de>
2022-08-21 17:24:31 +0200

    Add missing static query data to ClimbingLog story

--- src/components/post/ClimbingLog/index.stories.tsx
+++ src/components/post/ClimbingLog/index.stories.tsx
@@ -1,9 +1,34 @@
 import React from "react"
 import { ComponentStory } from "@storybook/react"
 import ClimbingLogComponent, { ClimbingLogProps } from "."
+
 export default {
   title: "Post/Climbing Log",
   component: ClimbingLogComponent,
+  parameters: {
+    staticQuery: {
+      allClimbingGradesCsv: {
+        nodes: [
+          {
+            YDS: "5.11d",
+            french: "7a+",
+            UIAA: "8+",
+            Fb_bloc: "5c",
+            Fb_trav: "5c+",
+            V: "V0",
+          },
+          {
+            YDS: "5.12a",
+            french: "7b",
+            UIAA: "8+/9-",
+            Fb_bloc: "6a",
+            Fb_trav: "6a+",
+            V: "V1",
+          },
+        ],
+      },
+    },
+  },
 }
 
 const Template: ComponentStory<typeof ClimbingLogComponent> = (
