2fa4c04fbdb0afad729cda2c1b2dfa24eaabd7da
Jan Gosmann <jan@hyper-world.de>
2022-10-02 16:59:55 +0200

    Fix module exclusions for SSR

--- gatsby-node.js
+++ gatsby-node.js
@@ -398,7 +398,7 @@ exports.createPages = args => {
 }
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
+  if (stage === "build-html" || stage === "develop-html") {
     actions.setWebpackConfig({
       module: {
         rules: [
