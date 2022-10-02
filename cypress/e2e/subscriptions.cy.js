3e9acc733b6b62d8a6f54286b2993db517a21f3b
Jan Gosmann <jan@hyper-world.de>
2022-10-02 18:24:28 +0200

    Update to Cypress 10

---
+++ cypress/e2e/subscriptions.cy.js
@@ -0,0 +1,101 @@
+import randomEmail from "random-email"
+
+describe("The subscription process", function () {
+  it("successfully loads", function () {
+    const email = randomEmail()
+
+    cy.on("window:before:load", win => {
+      const originalFetch = win.fetch
+      win.fetch = (url, options) => {
+        return originalFetch(
+          url.replace(
+            "https://doveseed.adventures.jgosmann.de",
+            "http://127.0.0.1:5000"
+          ),
+          options
+        )
+      }
+
+      let captchaSuccessCallback
+      win.grecaptcha = {
+        execute: () => captchaSuccessCallback("dummyCaptcha"),
+        render: (elem, { callback }) => {
+          captchaSuccessCallback = callback
+          return "widgetId"
+        },
+      }
+    })
+
+    cy.visit("/subscribe")
+    cy.window()
+      .its("onRecaptchaLoad")
+      .then(fn => fn())
+
+    cy.get("form.email-submission-form").within(() => {
+      cy.get('input[type="email"]').type(email)
+      cy.root().submit()
+      cy.get("button svg").should("have.attr", "data-icon", "check")
+    })
+
+    cy.readFile("../doveseed/doveseed-db.dev.json").then(content => {
+      const entity = Object.values(content._default).filter(
+        x => x.email === email
+      )[0]
+      const encodedEmail = encodeURIComponent(entity.email)
+      const token = encodeURIComponent(entity.confirm_token.data)
+      cy.visit(`/subscribe/confirm?email=${encodedEmail}&token=${token}`)
+    })
+
+    cy.get('form.email-submission-form input[type="email"]').should(
+      "have.value",
+      email
+    )
+    cy.get("form.email-submission-form").within(() => {
+      cy.root().submit()
+      cy.get("button svg").should("have.attr", "data-icon", "check")
+    })
+
+    cy.readFile("../doveseed/doveseed-db.dev.json").then(content => {
+      const entity = Object.values(content._default).filter(
+        x => x.email == email
+      )[0]
+      expect(entity.state).to.equal("subscribed")
+    })
+
+    cy.visit("/unsubscribe")
+    cy.window()
+      .its("onRecaptchaLoad")
+      .then(fn => fn())
+
+    cy.get("form.email-submission-form").within(() => {
+      cy.get('input[type="email"]').type(email)
+      cy.root().submit()
+      cy.get("button svg").should("have.attr", "data-icon", "check")
+    })
+
+    cy.readFile("../doveseed/doveseed-db.dev.json").then(content => {
+      const entity = Object.values(content._default).filter(
+        x => x.email === email
+      )[0]
+      const encodedEmail = encodeURIComponent(entity.email)
+      const token = encodeURIComponent(entity.confirm_token.data)
+      cy.visit(`/subscribe/confirm?email=${encodedEmail}&token=${token}`)
+    })
+
+    cy.get('form.email-submission-form input[type="email"]').should(
+      "have.value",
+      email
+    )
+    cy.get("form.email-submission-form").within(() => {
+      cy.root().submit()
+      cy.get("button svg").should("have.attr", "data-icon", "check")
+    })
+
+    cy.readFile("../doveseed/doveseed-db.dev.json").then(content => {
+      const entity = Object.values(content._default).filter(
+        x => x.email == email
+      )[0]
+      expect(entity).to.be.undefined
+    })
+  })
+})
