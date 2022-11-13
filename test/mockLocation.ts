07a31818e78dbd7b2135b34e8df6f06918ac5ce9
Jan Gosmann <jan@hyper-world.de>
2022-11-13 17:37:48 +0100

    Update dependencies

--- test/mockLocation.ts
+++ test/mockLocation.ts
@@ -1,9 +1,10 @@
-export const mockLocation = (url: string) =>
+export const mockLocation = (url: string): Location =>
   Object.assign(new URL(url), {
     ancestorOrigins: {
       length: 0,
       contains: jest.fn(),
       item: jest.fn(),
+      [Symbol.iterator]: jest.fn(),
     },
     assign: jest.fn(),
     reload: jest.fn(),
