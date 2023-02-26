8b0ff9c549aa2e3fca37cd3f3d1d5c2638b35c14
Jan Gosmann <jan@hyper-world.de>
2023-02-26 10:55:09 +0100

    Add E2E test for search

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,22 @@
+describe("The search", function () {
+  it("finds entries", function () {
+    cy.on("window:before:load", win => {
+      const originalFetch = win.fetch
+      win.fetch = (url: string, options) => {
+        return originalFetch(
+          url.replace(
+            "https://search.adventures.jgosmann.de",
+            "http://127.0.0.1:4000"
+          ),
+          options
+        )
+      }
+    })
+
+    cy.visit("/")
+    cy.get('input[type="search"]').first().type("Zugspitz")
+    cy.get('button[type="submit"]').first().click()
+
+    cy.get("main ol li h2").should("have.text", "Zugspitz crossing")
+  })
+})
