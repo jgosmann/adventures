cf918027fe8ed9d3f7c63542a89b6aa9fe58a300
Jan Gosmann <jan@hyper-world.de>
2023-09-16 19:32:14 +0200

    Allow smaller videos by using <Gallery> tag

--- src/components/post/Video.tsx
+++ src/components/post/Video.tsx
@@ -1,23 +1,28 @@
-import React from "react"
+import React, { useContext } from "react"
+import { GalleryContext } from "./Gallery"
 
 export interface VideoProps {
   src: string
 }
 
-const Video = ({ src }: VideoProps) => (
-  <div css={{ textAlign: "center", fontSize: 0, margin: 32 }}>
-    <video
-      controls
-      css={{
-        maxHeight: "80vh",
-        maxWidth: "100%",
-        boxShadow: "0 1px 2px rgba(0, 0, 0, 0.5)",
-        borderRadius: 2,
-      }}
-    >
-      <source src={src} type="video/mp4" />
-    </video>
-  </div>
-)
+const Video = ({ src }: VideoProps) => {
+  const galleryContext = useContext(GalleryContext)
+
+  return (
+    <div css={{ textAlign: "center", fontSize: 0, margin: 32 }}>
+      <video
+        controls
+        css={{
+          maxHeight: galleryContext.active ? "40vh" : "80vh",
+          maxWidth: "100%",
+          boxShadow: "0 1px 2px rgba(0, 0, 0, 0.5)",
+          borderRadius: 2,
+        }}
+      >
+        <source src={src} type="video/mp4" />
+      </video>
+    </div>
+  )
+}
 
 export default Video
