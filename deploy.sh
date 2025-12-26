25df2ca095bd5412f144d08593ebf276d832f098
Jan Gosmann <jan@hyper-world.de>
2025-12-26 12:48:34 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-#rsync -avz --delete public/ jgosmann@hyper-world.de:~/adventures
 npx netlify deploy --prod -d public/
-ssh jgosmann@hyper-world.de passenger-config restart-app /var/customers/webs/jgosmann/adventures-search
+ssh jgosmann@jgosmann.de sudo systemctl restart adventures-search
