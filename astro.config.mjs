8a4252d2301f4358e0b8870426fd51c5ba292cef
Jan Gosmann <jan@hyper-world.de>
2025-12-31 13:40:29 +0100

    Implement Titlescreen with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import mdx from "@astrojs/mdx"
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+  image: {
+    breakpoints: [750, 1080, 1280, 1668, 2048],
+  },
   vite: {
     server: {
       fs: {
