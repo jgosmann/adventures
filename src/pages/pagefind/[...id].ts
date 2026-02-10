50967eb42979d7e0a8f1bb15a1473a8bcf9d298b
Jan Gosmann <jan@hyper-world.de>
2026-02-10 20:05:12 +0100

    Fix search in production build

--- src/pages/pagefind/[...id].ts
+++ src/pages/pagefind/[...id].ts
@@ -1,10 +1,45 @@
 import { getCollection } from "astro:content"
+import * as pagefind from "pagefind"
+import { plaintext } from "../../mdast"
+import { reverseGeocode } from "../../geocode"
 
 export async function getStaticPaths() {
-  const searchFragments = await getCollection("searchFragments")
-  return searchFragments.map(fragment => ({
-    params: { id: fragment.id },
-    props: { content: fragment.data.content },
+  const [posts, { index }] = await Promise.all([
+    getCollection("posts"),
+    pagefind.createIndex({}),
+  ])
+  if (!index) {
+    throw new Error("Failed to create search index")
+  }
+  await Promise.all(
+    posts.map(async post => {
+      const [lat, long] = JSON.parse(`[${post.data.map}]`)
+      await index.addCustomRecord({
+        url: `/posts/${post.id}`,
+        content: [
+          post.data.title,
+          await reverseGeocode({ lat, long }),
+          post.data.categories.join(", "),
+          plaintext(post.body ?? ""),
+        ].join("\n"),
+        language: "en",
+        meta: {
+          id: post.id,
+          title: post.data.title,
+        },
+        sort: {
+          date: post.data.date.getTime().toString(),
+        },
+      })
+    })
+  )
+  const { errors, files } = await index.getFiles()
+  if (errors.length > 0) {
+    throw new Error(`Indexing errors: ${errors}`)
+  }
+  return files.map(file => ({
+    params: { id: file.path },
+    props: { content: file.content },
   }))
 }
 
