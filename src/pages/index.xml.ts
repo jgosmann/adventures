467625f858a77af184c54b9a84394d190e70bdfd
Jan Gosmann <jan@hyper-world.de>
2026-02-12 21:55:18 +0100

    Fix images in RSS feed

--- src/pages/index.xml.ts
+++ src/pages/index.xml.ts
@@ -1,6 +1,7 @@
 import rss from "@astrojs/rss"
 import { getCollection } from "astro:content"
 import { excerpt } from "../mdast"
+import { getImage } from "astro:assets"
 
 export async function GET(context) {
   const posts = (await getCollection("posts")).sort(
@@ -11,15 +12,24 @@ export async function GET(context) {
     description:
       "Jan's blog about adventures experienced outdoors and while travelling.",
     site: context.site,
-    items: posts.map(post => {
-      return {
-        title: post.data.title,
-        description: excerpt(post.body ?? ""),
-        pubDate: post.data.publishdate,
-        link: `/posts/${post.id}`,
-        categories: post.data.categories,
-      }
-    }),
+    items: await Promise.all(
+      posts.map(async post => {
+        return {
+          title: post.data.title,
+          description: excerpt(post.body ?? ""),
+          pubDate: post.data.publishdate,
+          link: `/posts/${post.id}`,
+          categories: post.data.categories,
+          customData: `<og:image>${new URL(
+            (await getImage({ src: post.data.background, width: 800 })).src,
+            context.site
+          )}</og:image>`,
+        }
+      })
+    ),
+    xmlns: {
+      og: "http://ogp.me/ns#",
+    },
     customData: `<lastBuildDate>${new Date(
       Date.now()
     ).toUTCString()}</lastBuildDate>`,
